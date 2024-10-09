2a3e26217d83ed9289e31182630d57305bb64751
Tanase <RobertTanase7999@gmail.com>
2024-10-09 08:24:32 -0400

    Colorized

--- CastleBravo.py
+++ CastleBravo.py
@@ -1,8 +1,17 @@
+
+
+
 import sys  # Importing the sys module to access system-specific parameters and functions
 import time  # Importing the time module to add delays (simulate loading time)
 
-# Print the welcome message at the start
-print("\nWelcome to InfoTechCenter V1.0\n")
+# ANSI escape sequences for colors
+RESET = "\033[0m"  # Reset to default color
+GREEN = "\033[32m"  # Green text
+YELLOW = "\033[33m"  # Yellow text
+RED = "\033[31m"  # Red text for warnings/errors
+
+# Print the welcome message at the start with color
+print(GREEN + "\nWelcome to InfoTechCenter V1.0\n" + RESET)
 
 x = 0  # Initial counter for the number of iterations
 ellipsis = 0  # Counter for the number of dots in the ellipsis animation
@@ -11,7 +20,7 @@ ellipsis = 0  # Counter for the number of dots in the ellipsis animation
 while x != 20:
     x += 1  # Increment the counter for each iteration
     # Create the message that includes the animated ellipsis (increases the number of dots each time)
-    message = ("InfoTech Center System Booting" + "." * ellipsis)
+    message = (YELLOW + "InfoTech Center System Booting" + "." * ellipsis + RESET)
 
     # Increment the ellipsis counter for the next dot
     ellipsis += 1
@@ -26,6 +35,6 @@ while x != 20:
     if ellipsis == 4:
         ellipsis = 0
 
-    # Once 20 iterations have passed, print the final message
-    if x == 20:
-        print("\n\nOperating System Booted Up - Retina Scanned - Access Granted")
+# Once 20 iterations have passed, print the final message in green
+if x == 20:
+    print("\n\n" + GREEN + "Operating System Booted Up - Retina Scanned - Access Granted" + RESET)
